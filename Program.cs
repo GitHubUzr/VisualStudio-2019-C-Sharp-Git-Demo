1477c4f6eb20aff90cd5d66f6c3cb90483510102
elizabeth109 <elizabeth109@mail.usf.edu>
2019-08-29 13:32:17 -0400

    Commit the change

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio_2019_C_Sharp_Git_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

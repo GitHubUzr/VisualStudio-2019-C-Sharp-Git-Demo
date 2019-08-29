90f8d2129085c04d1d78f13c10a001c1528ef4b2
elizabeth109 <elizabeth109@mail.usf.edu>
2019-08-29 14:12:36 -0400

    Added user input lines 13-17

--- Program.cs
+++ Program.cs
@@ -7,6 +7,14 @@ namespace VisualStudio_2019_C_Sharp_Git_Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Here is a change");
+
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            //Console.WriteLine("Hello " + name + "!");
+            Console.WriteLine("Hello {0}!", name);
         }
     }
 }

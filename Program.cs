d907519dd1ad275552585bf61e1e7cc1283312ee
elizabeth109 <elizabeth109@mail.usf.edu>
2019-09-05 20:03:43 -0400

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+ * This C# Console Application code is meant to follow
+ * Hello World project as well as receive
+ * user input and respond with user name
+ */
+
+using System;
 
 namespace VisualStudio_2019_C_Sharp_Git_Demo
 {
@@ -10,10 +16,20 @@ namespace VisualStudio_2019_C_Sharp_Git_Demo
 
             Console.WriteLine("Here is a change");
 
-            string name;
+
+            //asks for user input
             Console.WriteLine("What is your name?");
+            Console.WriteLine("Enter in the line below:");
+
+            //receives user input
+            string name;
             name = Console.ReadLine();
-            //Console.WriteLine("Hello " + name + "!");
+
+            /*
+             * This is another way to combine literal and variable
+             * Console.WriteLine("Hello " + name + "!");
+             */
+            //returns name in message
             Console.WriteLine("Hello {0}!", name);
         }
     }
